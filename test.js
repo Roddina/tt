71adc5d29884da21b520d51304d4c4aa9a614697
Rodina Mohamed <rodina2150@gmail.com>
2026-03-01 02:14:06 +0200

    Add portfolio.html and CSS

---
+++ test.js
@@ -0,0 +1,35 @@
+// function check(number) {
+//     console.log(number % 2 === 0 ? "Even" : "Odd");
+// }
+
+// checkEvenOrOdd(10); 
+// checkEvenOrOdd(3);  
+
+
+
+
+// function check(number) {
+//     if (number % 2 === 0) {
+//         console.log("Even");
+//     } else {
+//         console.log("Odd");
+//     }
+// }
+
+
+// checkEvenOrOdd(4); 
+// checkEvenOrOdd(7); 
+
+
+
+const functionn= (num) => {
+    return num * 2;
+};
+
+console.log(functionn(5)); 
+
+
+const checkEvenOrOdd = (num) => ( num % 2 === 0 ? "Even" : "Odd");
+
+console.log(checkEvenOrOdd(6)); 
+console.log(checkEvenOrOdd(9)); 
